8bb2e858e2405f36ff9db5bc620fab07aa17acc1
James Nail <james.nail@socialtables.com>
2024-01-24 13:33:25 -0500

    match msw jest examples

--- normal.test.js
+++ normal.test.js
@@ -2,16 +2,31 @@ const supertest = require("supertest");
 const { http, HttpResponse } = require("msw");
 const { setupServer } = require("msw/node");
 
-test("normal json response", async () => {
-  // Provide the server-side API with the request handlers.
-  const server = setupServer(
+let server;
+
+beforeAll(() => {
+  server = setupServer(
     http.get("http://localhost/json-example", () => {
       return HttpResponse.json({ foo: "bar" });
     })
   );
 
-  // Start the interception.
+  // Enable API mocking before all the tests.
   server.listen();
+});
 
+afterEach(() => {
+  // Reset the request handlers between each test.
+  // This way the handlers we add on a per-test basis
+  // do not leak to other, irrelevant tests.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Finally, disable API mocking after the tests are done.
+  server.close();
+});
+
+test("normal json response", async () => {
   await supertest("http://localhost").get("/json-example").expect(200);
 });
--- repro.test.js
+++ repro.test.js
@@ -4,9 +4,10 @@ const { setupServer } = require("msw/node");
 
 const encoder = new TextEncoder();
 
-test("reproduce openHandles issue", async () => {
-  // Provide the server-side API with the request handlers.
-  const server = setupServer(
+let server;
+
+beforeAll(() => {
+  server = setupServer(
     http.get("http://localhost/video", () => {
       const stream = new ReadableStream({
         start(controller) {
@@ -27,8 +28,22 @@ test("reproduce openHandles issue", async () => {
     })
   );
 
-  // Start the interception.
+  // Enable API mocking before all the tests.
   server.listen();
+});
 
+afterEach(() => {
+  // Reset the request handlers between each test.
+  // This way the handlers we add on a per-test basis
+  // do not leak to other, irrelevant tests.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Finally, disable API mocking after the tests are done.
+  server.close();
+});
+
+test("reproduce openHandles issue", async () => {
   await supertest("http://localhost").get("/video").expect(200);
 });
